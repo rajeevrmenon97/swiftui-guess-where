9799909148bcffbabacba351a2da8f5487b3ed5e
Rajeev Menon <rajeevrmenon97@gmail.com>
2023-10-12 14:31:10 -0600

    Initial Commit

---
+++ GuessWhere/GuessWhereApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessWhereApp.swift
+//  GuessWhere
+//
+//  Created by Rajeev R Menon on 10/12/23.
+//
+
+import SwiftUI
+
+@main
+struct GuessWhereApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
